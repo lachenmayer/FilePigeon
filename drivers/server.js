b1618d408d4646e87340206b3a12947f78c8ecf7
Harry Lachenmayer <harrylachenmayer@gmail.com>
2017-03-12 00:59:18 +0000

    clean up download

--- drivers/server.js
+++ drivers/server.js
@@ -1,4 +1,5 @@
 const encodeUrl = require('encodeurl')
+const cuid = require('cuid')
 const finalHandler = require('finalhandler')
 const fs = require('fs')
 const http = require('http')
@@ -11,6 +12,7 @@ const action = require('../helpers/action')
 const ofType = require('../helpers/ofType')
 
 const userFacingName = 'FilePigeon Drop.zip'
+const downloadUrl = '/drop'
 
 module.exports = function serverDriver (action$) {
   return ofType(action$, 'server/start')
@@ -21,54 +23,23 @@ module.exports = function serverDriver (action$) {
 function serveFile (filePath) {
   return xs.create({
     start: listener => {
-      const zipUrl = encodeUrl('/' + userFacingName)
       const server = http.createServer((req, res) => {
-        const done = finalHandler(req, res)
+        const onError = finalHandler(req, res)
+        const download = downloadHandler(req, res)
+
         if (req.method === 'OPTIONS') {
           res.setHeader('Allow', 'HEAD, GET')
-          return done()
+          return onError()
         }
         if (['HEAD', 'GET'].indexOf(req.method) === -1) {
           res.setHeader('Allow', 'HEAD, GET')
-          return done({status: 405})
+          return onError({status: 405})
         }
 
         const url = parseUrl(req)
         const path = url.pathname
-        if (path === zipUrl) {
-          fs.stat(filePath, (err, stats) => {
-            if (err) {
-              return done(err)
-            }
-            const {mtime, size} = stats
-            res.setHeader('Content-Type', 'application/zip')
-            res.setHeader('Content-Length', size)
-            res.setHeader('Last-Modified', mtime.toUTCString())
-            if (req.method === 'HEAD') {
-              return
-            }
-
-            const progressStream = makeProgressStream({
-              length: size,
-              time: 100,
-            })
-            const fileStream = fs.createReadStream(filePath, {encoding: null})
-
-            fileStream.pipe(progressStream)
-            progressStream.pipe(res)
-
-            progressStream.on('progress', progress => {
-              listener.next(action('server/request/progress', progress))
-            })
-
-            fileStream.on('end', () => {
-              fileStream.unpipe(progressStream)
-              progressStream.unpipe(res)
-              res.end()
-              listener.next(action('server/request/done'))
-            })
-          })
-          return
+        if (path === downloadUrl) {
+          return download(filePath, listener.next, onError)
         }
 
         res.write('TODO')
@@ -85,4 +56,47 @@ function serveFile (filePath) {
       this.server.close()
     }
   })
+}
+
+function downloadHandler (req, res) {
+  return function (filePath, onUpdate, onError) {
+    const id = cuid()
+    onUpdate(action('server/request/start', {id}))
+    fs.stat(filePath, (err, stats) => {
+      if (err) {
+        return onError(err)
+      }
+
+      const {mtime, size} = stats
+
+      res.setHeader('Content-Type', 'application/zip')
+      res.setHeader('Content-Length', size)
+      res.setHeader('Content-Disposition', contentDisposition(userFacingName))
+      res.setHeader('Last-Modified', mtime.toUTCString())
+
+      if (req.method === 'HEAD') {
+        return res.end()
+      }
+
+      const progressStream = makeProgressStream({
+        length: size,
+        time: 100,
+      })
+      const fileStream = fs.createReadStream(filePath, {encoding: null})
+
+      fileStream.pipe(progressStream)
+      progressStream.pipe(res)
+
+      progressStream.on('progress', progress => {
+        onUpdate(action('server/request/progress', {id, progress}))
+      })
+
+      fileStream.on('end', () => {
+        fileStream.unpipe(progressStream)
+        progressStream.unpipe(res)
+        res.end()
+        onUpdate(action('server/request/done', {id}))
+      })
+    })
+  }
 }
